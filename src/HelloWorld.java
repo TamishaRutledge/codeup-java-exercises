9a3f7e2d23bd9612fb172bc55ef6819aa6dcdb20
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-16 16:52:44 -0500

    Java Syntax, Data Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,41 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+        String myString = "My First Java String!";
+        System.out.println(myString);
+//        long myNumber;
+//        System.out.println(myNumber);
+//        myNumber = 3.14;
+//        myNumber= 123L;
+//        System.out.println(myNumber);
+//        myNumber = 123;
+//        System.out.println(myNumber);
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//        float myNumber = (float) 3.14;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//         x += 5;
+//         System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
     }
 }
