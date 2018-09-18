ef5e5fc404e0ec1f3e1330404566dcbc05e3864e
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-18 10:53:43 -0500

    Control Flow Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args){
@@ -28,12 +29,66 @@ public class ControlFlowExercises {
 //            i -= 5;
 //        }while (i >= -10);
 
-//        This does NOT work!! Infinate loop!
-        int i = 2;
-        do {
-            System.out.println("The value of i is: " + i);
-            i *= i;
-        }while (i < 1000000);
+//        Needed to use a long data type instead of an int!
+//        long i = 2;
+//        do {
+//            System.out.println("The value of i is: " + i);
+//            i *= i;
+//        }while (i < 1000000);
+
+//--------------------REFACTOR USING A FOR LOOP---------------------------//
+
+//        Create an integer variable i with a value of 5.
+//        Create a for loop that runs so long as i is less than or equal to 15
+//        Each loop iteration, output the current value of i, then increment i by one.
+
+//        for (int i = 5; i <= 15; i++ ){
+//            System.out.println("The value of i is: " + i);
+//        }
+//        Create a for loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//        Create a for loop that starts at 2,
+//        and displays the number squared on each line while the number is less than 1,000,000.
+
+//        for (int i = 0; i <= 100; i += 2){
+//            System.out.println("The value of i is: " + i);
+//        }
+
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println("The value of i is: " + i);
+//        }
+
+//            for (long i = 2; i <= 1000000; i *= i){
+//                System.out.println("The value of i is: " + i);
+//            }
+
+//------------------------FIZZBUZZ--------------------------------//
+//        Write a program that prints the numbers from 1 to 100.
+//        For multiples of three print “Fizz” instead of the number
+//        For the multiples of five print “Buzz”.
+//        For numbers which are multiples of both three and five print “FizzBuzz”.
+
+//        for (int i = 1; i <= 100; i++){
+//            if(i % 15 == 0){
+//                System.out.println("FizzBuzz");
+//            } else if(i % 3 == 0){
+//                System.out.println("Fizz");
+//            } else if(i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+//        Prompt the user to enter an integer.
+//        Display a table of squares and cubes from 1 to the value entered.
+//        Ask if the user wants to continue.
+//        Assume that the user will enter valid data.
+//        Only continue if the user agrees to.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        int userInput = scanner.nextInt();
 
     }
 }
---
+++ src/PushUp.java
@@ -0,0 +1,16 @@
+public class PushUp {
+    public static void main(String[] args){
+        char letter = 'A';
+        System.out.println("The defined input is: " + letter + " And that is a char value.");
+        int PushUpNumber = 1000000;
+        System.out.println("The defined input is: " + PushUpNumber + " And that is an int value.");
+        String MyPushUpString = "This is a Java pushup!";
+        System.out.println("The defined input is: " + MyPushUpString + " And that is a string value.");
+        double TestNumber = 4.75;
+        System.out.println("The defined input is: " + TestNumber + " And that is a double value.");
+        boolean TestValue = true;
+        System.out.println("The defined input is: " + TestValue + " And that is a boolean value.");
+        float FloatVar = 3.98F;
+        System.out.println("The defined input is: " + FloatVar + " And that is a float value.");
+    }
+}
