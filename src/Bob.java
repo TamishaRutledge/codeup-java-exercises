73e5e2074d8e78f4d8aa51633896200dd50ae2d3
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-18 22:10:17 -0500

    Strings exercise, part deux

---
+++ src/Bob.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+//        String teenagerName = "Bob";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ask Bob a question.");
+        String userInput = scanner.nextLine();
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.equals("")) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever");
+        }
+    }
+}
+
