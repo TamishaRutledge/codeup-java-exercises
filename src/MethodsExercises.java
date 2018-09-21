8ad760e84e1fdeec1ddcb45f5e4b60dc5fad84af
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-20 19:56:36 -0500

    Methods

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
+    static Scanner scan = new Scanner(System.in).useDelimiter("\n");
 
-//-------------------PROBLEM 1-----------------------//
+    //-------------------PROBLEM 1-----------------------//
+//----------------METHOD OVERLOADING-----------------//
     public static void Addition(int i, int j) {
-        System.out.println(i+j);
+        System.out.println(i + j);
     }
 
     public static void Subtraction(int i, int j) {
         System.out.println(i - j);
     }
-//------------------Do the bonus for this one-----------//
+
+    //------------------Do the bonus for this one-----------//
     public static void Multiplication(int i, int j) {
         System.out.println(i * j);
     }
@@ -19,35 +22,60 @@ public class MethodsExercises {
         System.out.println(i / j);
     }
 
-    public static void Modulus(int i, int j){
+    public static void Modulus(int i, int j) {
         System.out.println(i % j);
     }
 
 //---------------------PROBLEM 2--------------------------//
 
-    public static int getInteger(int min, int max){
-        System.out.println("Please enter a number between 1 and 10.");
-        Scanner userInput = new Scanner(System.in);
-//        int input;
-        if (userInput >= min && userInput <= max){
-            return userInput;
-        }
-        else{
-            System.out.println("Number out of range, try again.");
-        }
-
+    public static int getInt() {
+        int number = scan.nextInt();
+        return number;
     }
 
+    public static long getInt(int min, int max) {
+        System.out.println("Please input a number between " + min + " and " + max);
+        int number = getInt();
+        // if the number is in the range of the min and max, then return it.
+        if (number >= min && number <= max) {
+            return number;
+        } else {
+            // otherwise, inform the user that their input was outside the range...
+            System.out.println(number + " is outside of the range of " + min + " and " + max);
+
+            return getInt(min, max);
+        }
+
+//--------------------PROBLEM 3--------------------------//
+
+
+        public static long factorial( int a){
+            int factorial = 1;
+
+            while (a > 1) {
+                factorial *= a;
+                a--;
+            }
+            return factorial;
+        }
+    }
 
     public static void main(String[] args) {
+
+
 //-------------------PROBLEM 1-------------------------//
         Addition(3, 4);
-        Subtraction(8,4);
-        Multiplication(7,7);
+        Subtraction(8, 4);
+        Multiplication(7, 7);
 //----------dividing by zero throws an error-----------//
-        Division(9,3);
-        Modulus(10,2);
+        Division(9, 3);
+        Modulus(10, 2);
 //-----------------PROBLEM 2--------------------------//
-        getInteger(1,10);
+
+        System.out.println(factorial(5));
+
     }
+
+
+
 }
