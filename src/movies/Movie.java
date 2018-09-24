85c61e1662efb234efa0394bde37a78767beb0b6
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-24 16:19:45 -0500

    Movie App Exercise

---
+++ src/movies/Movie.java
@@ -0,0 +1,28 @@
+package movies;
+
+public class Movie {
+
+    private String movie;
+
+    private String movieCategory;
+
+
+//ARE THESE CONSTRUCTORS ON THOSE ^^^^ PRIVATE FIELDS??????//
+    public Movie(movieName) {
+        this.movieName = movieName;
+    }
+
+    public Movie(movieCategory) {
+        this.movieCategory = movieCategory;
+    }
+
+//    GETTERS AND SETTERS NEEDED  //
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
