9254d5d5b0da19bbc1c93dc40fd6c7d63c98a39f
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-24 17:00:08 -0500

    Movie Application Exercise

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -2,26 +2,40 @@ package movies;
 
 public class Movie {
 
-    private String movie;
-
-    private String movieCategory;
+    private String name;
+    private String category;
 
 
 //ARE THESE CONSTRUCTORS ON THOSE ^^^^ PRIVATE FIELDS??????//
-    public Movie(movieName) {
-        this.movieName = movieName;
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
     }
 
-    public Movie(movieCategory) {
-        this.movieCategory = movieCategory;
-    }
 
 //    GETTERS AND SETTERS NEEDED  //
 
 
-    public static void main(String[] args) {
-
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+// Don't think I need this main on this class//
+//    public static void main(String[] args) {
+//
+//    }
 
 
 
