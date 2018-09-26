56140066a9f3d0042a096843998823ba2de78a1f
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-26 16:58:58 -0500

    HashMaps Exercise.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,4 +1,28 @@
 package grades;
 
+import java.util.HashMap;
+
 public class GradesApplication {
+
+    public static void main(String[] args){
+
+        HashMap<String, Student> students = new HashMap<>();
+
+        Student danny = new Student("danny");
+        danny.addGrade(76);
+        students.put("danTheMan", danny);
+
+        Student francis = new Student("francis");
+        francis.addGrade(86);
+        students.put("frankTheTank", francis);
+
+        Student teddy = new Student("teddy");
+        teddy.addGrade(76);
+        students.put("teddyTalkTech", teddy);
+
+        Student justin = new Student("justin");
+        justin.addGrade(76);
+        students.put("justInTime", justin);
+        
+    }
 }
