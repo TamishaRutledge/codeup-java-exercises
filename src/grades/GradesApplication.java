b0630c5ac32c819584997101498e44ff2304f19b
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-26 23:43:49 -0500

    Grades Application, in progree.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -10,19 +10,29 @@ public class GradesApplication {
 
         Student danny = new Student("danny");
         danny.addGrade(76);
+        danny.addGrade(86);
+        danny.addGrade(96);
         students.put("danTheMan", danny);
 
         Student francis = new Student("francis");
         francis.addGrade(86);
+        francis.addGrade(66);
+        francis.addGrade(56);
         students.put("frankTheTank", francis);
 
         Student teddy = new Student("teddy");
         teddy.addGrade(76);
+        teddy.addGrade(86);
+        teddy.addGrade(46);
         students.put("teddyTalkTech", teddy);
 
         Student justin = new Student("justin");
         justin.addGrade(76);
+        justin.addGrade(73);
+        justin.addGrade(80);
         students.put("justInTime", justin);
-        
+
+        System.out.println(students);
+
     }
 }
